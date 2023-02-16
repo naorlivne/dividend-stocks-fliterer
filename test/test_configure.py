e4839258301f5c2c8caa4ded6a820047275ef4e9
Naor Livne <naorlivne@gmail.com>
2023-02-17 00:21:53 +0200

    framework for config unit tests

---
+++ test/test_configure.py
@@ -0,0 +1,20 @@
+import unittest
+from dividend_stocks_filterer.configure import *
+
+
+class TestReadConfigurations(unittest.TestCase):
+
+    def test_read_configurations(self):
+        pass
+
+    def test_auth_enabled_true(self):
+        pass
+
+    def test_auth_enabled_false(self):
+        pass
+
+    def test_read_configurations_missing_key(self):
+        pass
+
+    def test_read_configurations_default_values(self):
+        pass
