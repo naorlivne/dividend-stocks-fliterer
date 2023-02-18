f331344fb03d595fca5e3c349bbc37e6d6e13b43
Naor Livne <naorlivne@gmail.com>
2023-02-18 16:17:22 +0200

    can now see the results

--- dividend_stocks_filterer/ui.py
+++ dividend_stocks_filterer/ui.py
@@ -1,13 +1,14 @@
 import streamlit as st
 from dividend_radar import *
+from filterers import *
 from configure import *
 
 
 configuration = read_configurations()
 
 radar_file = DividendRadar(
-    dividend_radar_url = configuration["dividend_radar_download_url"],
-    local_file = configuration["local_file_path"]
+    dividend_radar_url=configuration["dividend_radar_download_url"],
+    local_file=configuration["local_file_path"]
 )
 if radar_file.check_if_local_is_latest() is False:
     radar_file.download_latest_version()
@@ -16,3 +17,9 @@ radar_dict = radar_file.read_radar_file_to_dict()
 
 st.title('divifilter')
 st.text("Radar file date: " + radar_file.latest_local_version)
+
+filtered_radar_dict = radar_dict
+
+# TODO - insert all needed filters here
+
+st.dataframe(radar_dict_to_table(filtered_radar_dict))
