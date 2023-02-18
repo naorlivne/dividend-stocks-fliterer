21156c278f8249f6a0106bbabdeb322eb955e83f
Naor Livne <naorlivne@gmail.com>
2023-02-18 18:24:31 +0200

    sidebar

--- dividend_stocks_filterer/ui.py
+++ dividend_stocks_filterer/ui.py
@@ -15,14 +15,16 @@ if radar_file.check_if_local_is_latest() is False:
 
 starting_radar_dict = radar_file.read_radar_file_to_dict()
 
+st.set_page_config(layout="wide")
 st.title('divifilter')
 st.text("Radar file date: " + radar_file.latest_local_version)
 
 radar_dict_filtered = starting_radar_dict
 
 # TODO - insert all needed filters here
-min_streak_years = st.slider(label="select minimum number of years of dividend streaks to display", min_value=5,
-                             max_value=50, value=18, key="min_dividend_streak_years")
-radar_dict_filtered = filter_dividend_paid_years_in_row(radar_dict_filtered, min_streak_years)
+with st.sidebar:
+    min_streak_years = st.slider(label="select minimum number of years of dividend streaks to display", min_value=5,
+                                 max_value=50, value=18, key="min_dividend_streak_years")
+    radar_dict_filtered = filter_dividend_paid_years_in_row(radar_dict_filtered, min_streak_years)
 
-st.dataframe(radar_dict_to_table(radar_dict_filtered))
+st.dataframe(radar_dict_to_table(radar_dict_filtered), use_container_width=True)
