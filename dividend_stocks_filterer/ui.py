aa122ad5c9550961a2632b995634e52e10c16725
Naor Livne <naorlivne@gmail.com>
2023-02-18 15:16:57 +0200

    adding showing the radar file to ui

--- dividend_stocks_filterer/ui.py
+++ dividend_stocks_filterer/ui.py
@@ -11,7 +11,8 @@ radar_file = DividendRadar(
 )
 if radar_file.check_if_local_is_latest() is False:
     radar_file.download_latest_version()
-    radar_dict = radar_file.read_radar_file_to_dict()
+
+radar_dict = radar_file.read_radar_file_to_dict()
 
 st.title('divifilter')
 st.text("Radar file date: " + radar_file.latest_local_version)
