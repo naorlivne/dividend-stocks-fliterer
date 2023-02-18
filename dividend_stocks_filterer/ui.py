c9e9e5c1ac39149c514a25cd99fc14a6ab8dab11
Naor Livne <naorlivne@gmail.com>
2023-02-18 18:10:09 +0200

    streak slider

--- dividend_stocks_filterer/ui.py
+++ dividend_stocks_filterer/ui.py
@@ -13,13 +13,16 @@ radar_file = DividendRadar(
 if radar_file.check_if_local_is_latest() is False:
     radar_file.download_latest_version()
 
-radar_dict = radar_file.read_radar_file_to_dict()
+starting_radar_dict = radar_file.read_radar_file_to_dict()
 
 st.title('divifilter')
 st.text("Radar file date: " + radar_file.latest_local_version)
 
-filtered_radar_dict = radar_dict
+radar_dict_filtered = starting_radar_dict
 
 # TODO - insert all needed filters here
+min_streak_years = st.slider(label="select minimum number of years of dividend streaks to display", min_value=5,
+                             max_value=50, value=18, key="min_dividend_streak_years")
+radar_dict_filtered = filter_dividend_paid_years_in_row(radar_dict_filtered, min_streak_years)
 
-st.dataframe(radar_dict_to_table(filtered_radar_dict))
+st.dataframe(radar_dict_to_table(radar_dict_filtered))
