7b13fe4a916c77fd3095be5529f31a7fd290812a
Naor Livne <naorlivne@gmail.com>
2023-04-17 01:30:13 +0300

    returing datetime when yfinance is queried as prep to print it on screen

--- test/test_yahoo_finance.py
+++ test/test_yahoo_finance.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 from dividend_stocks_filterer.yahoo_finance import *
 
@@ -5,7 +6,7 @@ from dividend_stocks_filterer.yahoo_finance import *
 class TestReadConfigurations(unittest.TestCase):
 
     def test_get_yahoo_finance_data_for_tickers_tuple(self):
-        reply = get_yahoo_finance_data_for_tickers_tuple(("PG", "SCL"))
+        time_reply, reply = get_yahoo_finance_data_for_tickers_tuple(("PG", "SCL"))
 
     def test_get_yahoo_finance_data_for_tickers_list(self):
-        reply = get_yahoo_finance_data_for_tickers_list(["PG", "SCL"])
+        time_reply, reply = get_yahoo_finance_data_for_tickers_list(["PG", "SCL"])
